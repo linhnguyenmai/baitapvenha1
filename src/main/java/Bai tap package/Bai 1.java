fe3ef607a2877feeeeba62ed15274e58c92520d7
linhnguyenmai <81745605+linhnguyenmai@users.noreply.github.com>
2024-08-30 16:13:06 +0700

    chua bai tap ve nha, bai 1

--- src/main/java/Bai tap package/Bai 1.java
+++ src/main/java/Bai tap package/Bai 1.java
@@ -1,17 +1,33 @@
 import java.util.Scanner;
 class Bai1  {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
         
-        System.out.println("Nhap so nguyen bat ky: ");
-        int x = sc.nextInt();
+    //     System.out.println("Nhap so nguyen bat ky: ");
+    //     int x = sc.nextInt();
         
-        int square = x*x;
-        System.out.println("Binh phuong so " + x + " la:" + square);
+    //     int square = x*x;
+    //     System.out.println("Binh phuong so " + x + " la:" + square);
         
-        int cube = x*x*x;
-        System.out.println("Luy thua 3 cua so "+x+" la: "+ cube);
+    //     int cube = x*x*x;
+    //     System.out.println("Luy thua 3 cua so "+x+" la: "+ cube);
         
     //     sc.close();
-    }
+    // }
+
+// chữa bài tập về nhà
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Nhap so nguyen bat ky: ");
+    int inputNumber = sc.nextInt();
+
+    int square = (int) Math.pow(inputNumber, 2);
+    System.out.println("Binh phuong so " + inputNumber + " la:" + square);
+
+    int cube = (int) Math.pow(inputNumber, 3);
+    System.out.println("Luy thua 3 cua so " + inputNumber + " la: " + cube);
+
+     sc.close();
+}
 }
